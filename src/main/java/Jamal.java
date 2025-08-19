b8327341105b943df780c9a785deb3230999a39b
KH-boop-bit <kenghong0202@gmail.com>
2025-08-19 15:57:56 +0800

    Level 0
    Rename file to Jamal
    Change print text

---
+++ src/main/java/Jamal.java
@@ -0,0 +1,12 @@
+public class Jamal {
+    public static void main(String[] args) {
+        String logo =
+        "____________________________________________________________\n"
+        + "Hello! I'm Jamal\n"
+        + "What can I do for you?\n"
+        + "____________________________________________________________\n"
+        + "Bye. Hope to see you again soon!\n"
+        + "____________________________________________________________";
+        System.out.println(logo);
+    }
+}
