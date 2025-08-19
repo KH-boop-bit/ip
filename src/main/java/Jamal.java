d957c362aeed2dc5d5c25e7c9fd1ab5a66def076
KH-boop-bit <kenghong0202@gmail.com>
2025-08-19 17:17:26 +0800

    Level-2
    Added list

--- src/main/java/Jamal.java
+++ src/main/java/Jamal.java
@@ -17,6 +17,9 @@ public class Jamal {
 
         Scanner scanner = new Scanner(System.in);
 
+        String[] inputlist = new String[100];
+        int inputlistcounter = 0; //global counter for number of list items
+
         while (true) { //keep the while loop active until exit string is executed
             System.out.println(""); //empty string placeholder for input
             String input = scanner.nextLine();
@@ -24,10 +27,23 @@ public class Jamal {
             if (input.toLowerCase().equals("bye")) { //ignorecase so all Bye bye works
                 break;
             }
+
+            if (input.toLowerCase().equals("list")) {
+                System.out.println("____________________________________________________________\n");
+                for (int i = 0; i < inputlistcounter; i++) {
+                    System.out.println(
+                            (i + 1) + ". "
+                            + inputlist[i] + "\n");
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
             System.out.println(
                     "____________________________________________________________\n"
-                    + input + "\n"
+                    + "added: " + input + "\n"
                     + "____________________________________________________________");
+            inputlist[inputlistcounter] = input; //put input into array first
+            inputlistcounter++; //maintain a global counter
         }
         scanner.close(); //release system resources
         System.out.println(exit);
