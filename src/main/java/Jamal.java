5cdc137682b812a70546146b3f5d994505193c2f
KH-boop-bit <kenghong0202@gmail.com>
2025-08-19 16:47:29 +0800

    Level-1
    Added inputs for Jamal Bot

--- src/main/java/Jamal.java
+++ src/main/java/Jamal.java
@@ -1,12 +1,35 @@
+import java.util.Scanner; //import scanner class for inputs
+
 public class Jamal {
     public static void main(String[] args) {
-        String logo =
-        "____________________________________________________________\n"
-        + "Hello! I'm Jamal\n"
-        + "What can I do for you?\n"
-        + "____________________________________________________________\n"
-        + "Bye. Hope to see you again soon!\n"
-        + "____________________________________________________________";
-        System.out.println(logo);
+        String welcome =
+            "____________________________________________________________\n"
+            + "Hello! I'm Jamal\n"
+            + "What can I do for you?\n"
+            + "____________________________________________________________\n";
+
+        String exit =
+                "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+
+        System.out.println(welcome);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) { //keep the while loop active until exit string is executed
+            System.out.println(""); //empty string placeholder for input
+            String input = scanner.nextLine();
+
+            if (input.toLowerCase().equals("bye")) { //ignorecase so all Bye bye works
+                break;
+            }
+            System.out.println(
+                    "____________________________________________________________\n"
+                    + input + "\n"
+                    + "____________________________________________________________");
+        }
+        scanner.close(); //release system resources
+        System.out.println(exit);
     }
 }
